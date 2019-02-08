0126edd5b4eab2cc1f5e526d22190efa92309057
Josef Lexa <lexajosef94@gmail.com>
2019-02-08 19:05:16 +0100

    Accessible registration component

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { UserService } from '../_services/user.service';}
+import { UserService } from '../_services/user.service';
+import { AuthService } from '../_services/auth.service';
 import { User } from '../_models/user';
 
 @Component({
@@ -19,12 +20,19 @@ export class RegistrationComponent implements OnInit {
 
   regForm: FormGroup;
   errorMsg: string;
+  emailErrMsg: string;
+  passwordConfMsg: string;
   submitted: boolean = false;
   loading: boolean = false;
+  showNameError: boolean = false;
+  showEmailError: boolean = false;
+  showPasswordError: boolean = false;
+  showPasswordConfError: boolean = false;
 
   constructor(
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
@@ -39,21 +47,54 @@ export class RegistrationComponent implements OnInit {
   get formControls() { return this.regForm.controls; }
 
   private onFormInvalid() {
-    // set described by error
+    // name control error check
     if (this.formControls.name.invalid) {
       this.nameEl.nativeElement.setAttribute('aria-describedby', 'nameError');
-    }
-    if (this.formControls.email.invalid) {
-      this.emailEl.nativeElement.setAttribute('aria-describedby', 'emailError');
-    }
-    if (this.formControls.password.invalid) {
-      this.passwordEl.nativeElement.setAttribute('aria-describedby', 'passwordError');
-    }
-    if (this.formControls.passwordConfirm.invalid) {
-      this.passwordConfirmEl.nativeElement.setAttribute('aria-describedby', 'passwordConfirmError');
+      this.showNameError = true;
+    } else {
+      this.showNameError = false;
     }
 
-    // TODO: set focus in form
+    // email control error check
+    if (this.formControls.email.invalid) {
+      this.emailEl.nativeElement.setAttribute('aria-describedby', 'emailError');
+      this.showEmailError = true;
+      if (this.formControls.email.errors.required) {
+        this.emailErrMsg = 'Email is required.';
+      } else {
+        this.emailErrMsg = 'Invalid email format.';
+      }
+    } else {
+      this.showEmailError = false;
+    }
+
+    // password control error check
+    if (this.formControls.password.invalid) {
+      this.passwordEl.nativeElement.setAttribute('aria-describedby', 'passwordError');
+      this.showPasswordError = true;
+    } else {
+      this.showPasswordError = false;
+    }
+
+    // password confirm control error check
+    if (this.formControls.passwordConfirm.invalid) {
+      this.passwordConfirmEl.nativeElement.setAttribute('aria-describedby', 'passwordConfirmError');
+      this.showPasswordConfError = true;
+      this.passwordConfMsg = 'Password must contain at least 5 characters.';
+    } else {
+      this.showPasswordConfError = false;
+    }
+
+    // set focus on first invalid form control
+    if (this.formControls.name.invalid) {
+      this.nameEl.nativeElement.focus();
+    } else if (this.formControls.email.invalid) {
+      this.emailEl.nativeElement.focus();
+    } else if (this.formControls.password.invalid) {
+      this.passwordEl.nativeElement.focus();
+    } else {
+      this.passwordConfirmEl.nativeElement.focus();
+    }
   }
 
   private onFormValid() {
@@ -61,6 +102,14 @@ export class RegistrationComponent implements OnInit {
     this.emailEl.nativeElement.removeAttribute('aria-describedby');
     this.passwordEl.nativeElement.removeAttribute('aria-describedby');
     this.passwordConfirmEl.nativeElement.removeAttribute('aria-describedby');
+    this.showNameError = false;
+    this.showEmailError = false;
+    this.showPasswordError = false;
+    this.showPasswordConfError = false;
+  }
+
+  private passwordsMatch(): boolean {
+    return this.formControls.password.value === this.formControls.passwordConfirm.value;
   }
 
   private sendRegRequest() {
@@ -72,10 +121,7 @@ export class RegistrationComponent implements OnInit {
 
     this.userService.register(user)
       .subscribe(
-        resp => {
-          console.log(resp);
-          // TODO: login user and redirect to app component
-        },
+        resp => this.sendLoginRequest(),
         err => {
           if (err.status === 400) {
             this.errorMsg = 'User with this e-mail is already registred.';
@@ -90,6 +136,16 @@ export class RegistrationComponent implements OnInit {
       );
   }
 
+  private sendLoginRequest() {
+    this.authService.login(this.formControls.email.value, this.formControls.password.value)
+      .subscribe(
+        // user logged in, navigate to default route
+        token => this.router.navigate(['/']),
+        // login error, navigate to login for login user through that component
+        err => this.router.navigate(['/login'])
+      );
+  }
+
   onRegistration() {
     // reset form states
     this.submitted = true;
@@ -99,8 +155,13 @@ export class RegistrationComponent implements OnInit {
     if (this.regForm.invalid) {
       this.onFormInvalid();
       return;
-    } else {
+    } else if (this.passwordsMatch()) {
       this.onFormValid();
+    } else {
+      this.passwordConfMsg = 'Passwords don\'t match.';
+      this.showPasswordError = false; // show error only on confirm password input
+      this.showPasswordConfError = true;
+      return;
     }
 
     this.loading = true;
