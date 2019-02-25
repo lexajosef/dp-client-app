eb591bd3fa1ed46c71e6c15d3555ab63d794c219
Josef Lexa <lexajosef94@gmail.com>
2019-02-25 14:58:59 +0100

    Create functions for execution editor commands

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-editor',
@@ -7,9 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
+  @ViewChild('editor') editor: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+    this.editorDocument.designMode = 'on';
+    this.addStylesToEditor();
+  }
+
+  get editorDocument(): any { return (<any> this.editor.nativeElement).contentDocument }
+
+  private addStylesToEditor() {
+    let linkRobotoFont: HTMLLinkElement = this.editorDocument.createElement('link');
+    linkRobotoFont.rel = 'stylesheet';
+    linkRobotoFont.href = 'https://fonts.googleapis.com/css?family=Roboto:400,700';
+
+    let editorStyle: HTMLElement = this.editorDocument.createElement('style');
+    editorStyle.appendChild(document.createTextNode(`
+      body {
+        font-family: Roboto, sans-serif;
+      }`
+    ));
+
+    this.editorDocument.head.appendChild(linkRobotoFont);
+    this.editorDocument.head.appendChild(editorStyle);
+  }
+
+  private focusEditor(): void {
+    this.editorDocument.body.focus();
+  }
+
+  doEditorCommand(command: any, arg: any = null) {
+    let success: boolean;
+
+    console.log(`execution command ${command} with arg = ${arg}`);
+    try {
+      success = this.editorDocument.execCommand(command, false, arg);
+    } catch(error) {
+      alert(error);
+    }
+
+    if (!success) {
+      const supported = document.queryCommandSupported(command);
+      const msg = supported ? 'Unknown error. Is anything selected?' : 
+          'Command is not supported by your browser.';
+      alert(msg);
+    }
+
+    this.focusEditor();
   }
 
 }
