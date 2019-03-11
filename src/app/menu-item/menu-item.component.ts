3d4544cbd678977b35dd3429139f66ff34b196e6
Josef Lexa <lexajosef94@gmail.com>
2019-03-11 19:42:37 +0100

    Remove style of menu item

--- src/app/menu-item/menu-item.component.ts
+++ src/app/menu-item/menu-item.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, ViewChild, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'ui-menu-item',
-  styleUrls: ['./menu-item.component.css'],
   template: `
     <ng-template>
       <ng-content></ng-content>
