26b08126d9b07dfad4f6c22dbd47dfec40168244
Josef Lexa <lexajosef94@gmail.com>
2019-03-14 19:32:10 +0100

    Load more posts on home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as KEYCODES from 'keycode-js';
 
 import { AuthService } from '../_services/auth.service';
 import { PostsService } from '../_services/posts.service';
@@ -7,13 +8,16 @@ import { Post } from '../_models/post';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css', '../card/card.component.css']
 })
 export class HomeComponent implements OnInit {
 
   userName: string;
-  userPosts: Post[];
+  allUserPosts: Post[];
+  postsToView: Post[];
   latestPosts: Post[];
+
+  postToViewCount: number = 6;
 
   constructor(
     private authService: AuthService,
@@ -27,15 +31,26 @@ export class HomeComponent implements OnInit {
 
   getUserPosts(orderDesc: boolean) {
     this.postsService.getAllByUserId(this.authService.getUserTokenPayload().id, orderDesc)
-      .subscribe(posts => this.userPosts = posts);
+      .subscribe(posts => {
+        this.allUserPosts = posts;
+        this.postsToView = this.allUserPosts.slice(0, this.postToViewCount);
+      });
   }
 
   getLatestPosts() {
-    this.postsService.getLatestPosts(6, 0)
+    this.postsService.getLatestPosts(this.postToViewCount, 0)
       .subscribe(posts => this.latestPosts = posts);
   }
 
   orderUserPosts(desc: boolean) {
     this.getUserPosts(desc);
+  }
+  
+  showMorePosts(ev: any) {
+    if (ev.type === 'click' || (ev.type === 'keydown' && ev.keyCode === KEYCODES.KEY_RETURN)) {
+      if (this.allUserPosts.length > this.postsToView.length) {
+        this.postsToView = this.allUserPosts.slice(0, this.postsToView.length + this.postToViewCount);
+      }
+    }
   }
 }
